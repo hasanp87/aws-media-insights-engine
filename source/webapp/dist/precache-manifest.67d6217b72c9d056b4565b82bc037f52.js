e4297bb3ff99f01bb805a54398efbef9703e758a
joan <morjoan@amazon.com>
2019-11-14 16:42:09 -0800

    first go

---
+++ source/webapp/dist/precache-manifest.67d6217b72c9d056b4565b82bc037f52.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "08517ad0deb6f299b71f",
+    "url": "/css/app.21824a06.css"
+  },
+  {
+    "revision": "08517ad0deb6f299b71f",
+    "url": "/js/app.23db0d15.js"
+  },
+  {
+    "revision": "4fc342b7322399102840",
+    "url": "/js/chunk-274e15a0.a690a32d.js"
+  },
+  {
+    "revision": "bf92bd5f7449a0dce5dc",
+    "url": "/js/chunk-2d0e5b70.3a25ad31.js"
+  },
+  {
+    "revision": "da393f75905a9493328c",
+    "url": "/js/chunk-2d225b5b.f45bd73f.js"
+  },
+  {
+    "revision": "8f9a1d517d8729af33cb",
+    "url": "/js/chunk-2d2374d9.3eecb8e9.js"
+  },
+  {
+    "revision": "cbae6b74c904093456f4",
+    "url": "/js/chunk-452c4062.b5c39bf5.js"
+  },
+  {
+    "revision": "6ed42f33171ee46a73be",
+    "url": "/js/chunk-68185b22.3cdfb994.js"
+  },
+  {
+    "revision": "c503a15c7d9c123ea9c2",
+    "url": "/js/chunk-84a3645a.0c1870c7.js"
+  },
+  {
+    "revision": "02d97c30cc9045ac20d5",
+    "url": "/js/chunk-a353b2a6.1b932808.js"
+  },
+  {
+    "revision": "894725436f4e7b604342",
+    "url": "/css/chunk-vendors.09939d10.css"
+  },
+  {
+    "revision": "894725436f4e7b604342",
+    "url": "/js/chunk-vendors.aa3d4c27.js"
+  },
+  {
+    "revision": "46ac6629168546c71dbf115992aa4be9",
+    "url": "/fonts/VideoJS.46ac6629.eot"
+  },
+  {
+    "revision": "0dc185ffdbee7c70efd8ff628cd83530",
+    "url": "/index.html"
+  }
+];
---
+++ source/webapp/src/store/state.js
@@ -0,0 +1,7 @@
+export default {
+  player: null,
+  markers: [],
+  display_asset_id: '',
+  chart_tuples: [],
+  selected_label: '',
+}
